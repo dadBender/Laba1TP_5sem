c3b9a6e78ecb52cf6c196c6ffda9b5d6e8968823
Antnon Efimov <ntnefimov@gmail.com>
2023-10-20 19:23:27 +0300

    Second commit

--- laba1_5sem/laba1_5sem/main.cpp
+++ laba1_5sem/laba1_5sem/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
-	std::cout << "Hello world" << std::endl;
+	setlocale(LC_ALL, "Russian");
+
+	int button;
+	bool flag = true; 
+		while (flag) {
+			system("cls"); 
+			cout << "Меню: " << endl << endl;
+			cout << "1 - " << endl;
+			cout << "2 - " << endl;
+			
+			cout << "Выберите пункт: ";
+			cin >> button;
+			if (cin.fail()) { 
+				button = -1;
+				cin.clear();
+				cin.ignore(10000, '\n');
+			}
+			switch (button) {
+			case 1:
+				
+				
+					break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				flag = false; 
+				cout << "\nКонец" << endl;
+				break;
+			default:
+				cout << endl << "Некорректные данные, введите заново)" << endl
+					<< endl; 
+				
+				break;
+			}
+		}
+	return 0;
 }
