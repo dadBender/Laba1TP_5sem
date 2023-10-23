8ec7e7a883313e7971c71d7698cc97979afc03b3
Anton Efimov <106679332+dadBender@users.noreply.github.com>
2023-10-23 10:23:56 +0300

    Update and rename factory.cpp to ship.cpp

---
+++ laba1_5sem/laba1_5sem/ship.cpp
@@ -0,0 +1,12 @@
+#include "ship.h"
+#include <iostream>
+using namespace std;
+Factory::Factory()
+{
+
+	cout << "Âûçîâ êîíñòðóêòîðà (Àáñòðàêòíûé êëàññ)" << endl << endl;
+}
+Factory::~Factory()
+{
+	cout << "Âûçîâ äåñòðóêòîðà (Àáñòðàêòíûé êëàññ)" << endl << endl;
+}
